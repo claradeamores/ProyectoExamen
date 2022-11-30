4c8d98ef19feffd788a90177a5593dab6e4fe1bb
claradeamores <claradeamores@gmail.com>
2022-11-30 17:13:55 +0100

    segundo comentario

--- src/ProyectoEclipse.java
+++ src/ProyectoEclipse.java
@@ -4,6 +4,7 @@ public class ProyectoEclipse {
 	public static void main(String[] args) {
 		System.out.println("Esto nos va a dejar canijos y calvos");
 		System.out.println("hola");
+		System.out.println("2");
 
 	}
 
